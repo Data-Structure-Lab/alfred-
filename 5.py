127615d94df912cd9af8700a118fd41fcdeefe06
alfredkp1 <115702428+alfredkp1@users.noreply.github.com>
2022-11-25 11:50:33 +0530

    Delete 5.py

--- 5.py
+++
@@ -1,9 +0,0 @@
-string=input("enter string")
-word=input("enter a word")
-a=[]
-count=0
-a=string.split(" ")
-for i in range(0,len(a)):
-   if(word==a[i]):
-       count=count+1
-print("count of the word is:",count)
